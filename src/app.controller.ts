004a43f211da8a5642cd3c904ef0fae477b8b3d9
vishalpotinda2608 <vishalpotinda2608@gmail.com>
2023-02-25 21:08:22 +0530

    api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,15 +6,13 @@ import { Get, Inject } from '@nestjs/common/decorators';
 
 @Controller()
 export class AppController {
-  getHello(): any {
-    throw new Error('Method not implemented.');
-  }
+
   constructor(private readonly appservice: AppService) {}
   // @Inject('auth_microservices') privateclient: ClientProxy;
 
-  // @Get()
-  // async getHello(): Promise<any> {
-  //   const data = await this.privateclient.send('getregister', '');
-  //   return data;
-  // }
+  @Get()
+  async getHello(): Promise<any> {
+    
+    return "Welcome to server"
+  }
 }
